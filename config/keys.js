d7fa8b9590fb05cb76c2d624dcf9f0d723b25bd7
Robert Gant <demontegant@gmail.com>
2018-12-11 16:36:06 -0600

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://gant123:6701221m@ds021000.mlab.com:21000/storybook-dev',
+  googleClientID:'763991421912-rnjagtnnhp0di7pd1ake31i07d2qr17v.apps.googleusercontent.com',
+  googleClientSecret:'RJFiuPz3EuKjONDZFtrEtsJb'
+}
